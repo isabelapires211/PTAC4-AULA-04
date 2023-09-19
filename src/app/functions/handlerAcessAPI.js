866d738285c406de4cf4b1a4300fabf963c81b0b
Eoleonardo <>
2023-09-19 16:23:41 -0400

    commit

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -3,7 +3,7 @@ const usuarios = [
     {
         nome: "isabela",
         password:"123",
-        email:"isa@gmail.com",
+        email:"isabela@teste",
         token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
     },
     {
