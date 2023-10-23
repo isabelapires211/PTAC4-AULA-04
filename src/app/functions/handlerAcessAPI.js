71d9e22b4fad616a073e20a34583e2083b150366
isabelapires211 <piresisabela211@gmail.com>
2023-10-23 10:09:33 -0300

    commit

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -21,22 +21,18 @@ const url = "https://trabalho-final-seven.vercel.app/"
 
 
 const getUserAuthenticated = async (user) => {
-
-    const responseOfApi = await fetch (url+ 'user/authenticated',
-     {
-        method: "POST",
-        headers: { "Content-type" : "Application/json"},
-        body:JSON.stringify(user)
-     }
-     )
-
-     const userAuth = await responseOfApi.json()
-     return userAuth;
-
+    const responseOfApi = await fetch(url +"/user/authenticated",
+    {
+       method:"POST",
+       headers:{" Content-Type":"Application/json"},
+       body: JSON.stringify(user)
+    }
+    );
+   const userAuth =await responseOfApi.json();
+   return userAuth;
 }
 
 const getUsers = () =>{
-    return usuarios;
         
 }
 export { getUsers, getUserAuthenticated };
