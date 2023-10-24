6791447681caf20185a3684cc334020febdfb84c
isabelapires211 <piresisabela211@gmail.com>
2023-10-24 17:26:25 -0400

    commit

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,38 +1,40 @@
 'use server'
 
-import { headers } from "next/dist/client/components/headers"
 
-const usuarios = [
-    {
-        nome: "isabela",
-        password:"123",
-        email:"isabela@teste",
-        token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-    },
-    {
-        nome: "Gustavo Gomes",
-        password:"123",
-        email:"Gustavo@gmail.com",
-        token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-    }
-]
-
-const url = "https://trabalho-final-seven.vercel.app/"
+const url = "https://trabalho-final-tau.vercel.app"
 
 
 const getUserAuthenticated = async (user) => {
     const responseOfApi = await fetch(url +"/user/authenticated",
     {
+       cache:"no-cache",
        method:"POST",
-       headers:{" Content-Type":"Application/json"},
+       headers:{"Content-Type":"Application/json"},
        body: JSON.stringify(user)
     }
     );
    const userAuth =await responseOfApi.json();
+   console.log(userAuth)
    return userAuth;
 }
 
-const getUsers = () =>{
+const getUsers = async () =>{
+    try{
+        const responseOfApi = await fetch(url + "/users",{
+            next:{revalidate:10}
+        });
+        const listUsers = responseOfApi.json();
+
+        return listUsers
+    }catch{
+        return null;
+    }
+
+    }
+
+
+
+    
         
-}
+
 export { getUsers, getUserAuthenticated };
